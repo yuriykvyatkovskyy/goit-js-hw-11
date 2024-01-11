262e3ee3fe627b2d944dbfdacc90b0091c3c63a6
yuriykvyatkovskyy <yuriykreik20021997@gmail.com>
2024-01-11 05:47:32 +0200

    update js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('.form');
+const input = document.querySelector('input');
+const galleryBox = document.querySelector('.gallery-box');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+const searchParams = {
+    key: '41752354-d1ac8bee07efd7a3da621dba9',
+    q: '',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    nav: true,
+    captionDelay: 250,
+    captionsData: 'alt',
+    close: true,
+    enableKeyboard: true,
+    docClose: true,
+});
